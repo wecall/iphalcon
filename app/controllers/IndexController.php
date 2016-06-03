2a2c1f8f8ca6368efcd1479a09fba563f4ec5d14
ronchen <chenrongrong@51jk.com>
2016-06-03 16:20:36 +0800

    change file url

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -9,14 +9,16 @@ class IndexController extends Controller{
     }
 
     public function indexAction(){
-    //    $mongodb = new  MongoService("imovie","qrcodes");
-    //    // $mongodb = MongoService::getInstance("imovie","qrcodes");
-	   // var_dump($mongodb->count());
-	   // exit;
-       // 邮件服务测试      ----------------------------  显示成功但是没有收到邮件问题
-       $mail = new MailsService();
-       var_dump($mail->send("416994628@qq.com","测试邮件地址","<b>文本测试</b>"));
-       exit;
+      
+      // mongodb服务测试   ----------------------------------  成功
+      // $mongodb = new  MongoService("imovie","qrcodes");
+	    // var_dump($mongodb->count());
+	    // exit;
+      
+      // 邮件服务测试      ----------------------------  显示成功但是没有收到邮件问题
+      // $mail = new MailsService();
+      // var_dump($mail->send("416994628@qq.com","测试邮件地址","<b>文本测试</b>"));
+      // exit;
     }
 
 
