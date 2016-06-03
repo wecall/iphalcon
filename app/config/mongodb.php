141357ae033a8f59c94644d3776ac00b8d991dd3
ronchen <chenrongrong@51jk.com>
2016-06-03 15:06:54 +0800

    add mongodb service

---
+++ app/config/mongodb.php
@@ -0,0 +1,8 @@
+<?php
+//设置默认连接
+$config['host'] = "127.0.0.1";
+$config['port'] = "27017";
+$config['username'] = "";
+$config['password'] = "";
+
+return $config;
--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -9,8 +9,15 @@ class IndexController extends Controller{
     }
 
     public function indexAction(){
+       $mongodb = new  MongoService();
+       $mongodb->setDb("imovie","qrcodes");
+	   echo ($mongodb->mongodb->count());
+	   exit;
+       // 邮件服务测试      ----------------------------  显示成功但是没有收到邮件问题
        $mail = new MailsService();
        var_dump($mail->send("416994628@qq.com","测试邮件地址","<b>文本测试</b>"));
        exit;
     }
+
+
 }
