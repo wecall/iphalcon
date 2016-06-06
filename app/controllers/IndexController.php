b064a21dfcbbbc7752d522d7389f4abd4db6a863
ronchen <chenrongrong@51jk.com>
2016-06-06 18:03:35 +0800

    debug zhihu page curl

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -66,4 +66,39 @@ class IndexController extends Controller{
     }
 
 
+      public function testAction(){
+            $url = "https://www.zhihu.com/";
+
+            $ch = curl_init();
+            // 设置浏览器的特定header
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              "Host: www.zhihu.com",
+              "Connection: keep-alive",
+              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+              "Upgrade-Insecure-Requests: 1",
+              "DNT:1",
+              "Accept-Language: zh-CN,zh;q=0.8,en-GB;q=0.6,en;q=0.4,en-US;q=0.2",
+              'Cookie:d_c0="AIAAi_Rz-QmPTi0I_ivpxLCWD1C1KyuXeRs=|1464139326"; _za=6a4e0377-3a70-430e-8fd9-21a427eb3445; q_c1=10bc37a765244cdfb2f1189a6c3c5a77|1464139328000|1464139328000; l_cap_id="MjFlZGZhODBlMTNlNDE1Nzk1MDFiNTVlNDJkZmE4Yjg=|1465199185|354262a67255e87b552ddc113b24dc28f32cabdb"; cap_id="NTUwYTc5YTMwMmVkNDlhMmE2NDMyNmZjZjQ0MWZhZjU=|1465199185|8eb1fcf103e0bc54bb684f41df2a34d43fa5f172"; _zap=a2afe5a6-a55b-439d-9987-836f868cf3e1; login="NzJhNmM5NjRjZTllNGY0NWEwMjJkMzc3MzU1Yzc0ZDQ=|1465199194|ad689488ec6a24e27b38effe2021ff458e755bb2"; z_c0=Mi4wQUJETUJLdUtpUWdBZ0FDTDlIUDVDUmNBQUFCaEFsVk5XcmQ4VndCN3lLUm9ZcktMNDVuLTUtZElNWFRwd3VsdHd3|1465199194|e63d6010a81e23f4b93de286cc7980dac8f9858c; _xsrf=9c5e3e9576c417b83b71053cff18444c; a_t="2.0ABDMBKuKiQgXAAAAmdV8VwAQzASriokIAIAAi_Rz-QkXAAAAYQJVTVq3fFcAe8ikaGKyi-OZ_ufnSDF06cLpbcNfKSrsrtaeQFnO8oxXS8Vsv7cP6w=="; __utmt=1; __utma=51854390.2042775205.1465206941.1465206941.1465206941.1; __utmb=51854390.2.10.1465206941; __utmc=51854390; __utmz=51854390.1465206941.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=51854390.100-2|2=registration_date=20150813=1^3=entry_date=20150813=1',
+            ));
+            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0');
+            // 在HTTP请求头中"Referer: "的内容。
+            curl_setopt($ch, CURLOPT_REFERER,"https://www.baidu.com/s?word=%E7%9F%A5%E4%B9%8E&tn=sitehao123&ie=utf-8&ssl_sample=normal&f=3&rsp=0");
+            curl_setopt($ch, CURLOPT_ENCODING, "gzip, deflate, sdch");
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_TIMEOUT,120);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//302redirect
+            // 针对https的设置
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+            $html = curl_exec($ch);
+            curl_close($ch);
+            if($html === false) {
+              echo 'Curl error: ' . curl_error($ch) . "<br>\n\r";
+            } else {
+                  echo $html;
+            }
+      }
+
+
 }
