93f5b62dd200a170cb657897b764b885568a6443
ronchen <chenrongrong@51jk.com>
2016-06-06 17:11:25 +0800

    debug

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -17,7 +17,17 @@ class IndexController extends Controller{
       $curl->set_cookie($cookie);
       $curl->set_gzip(true);
       $url = "http://www.zhihu.com/people/dai-shu-qiong/about";
-      $curl->get($url);
+      $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,"; 
+      $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"; 
+      $header[] = "Cache-Control: max-age=0"; 
+      $header[] = "Connection: keep-alive"; 
+      $header[] = "Keep-Alive: 300"; 
+      $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; 
+      $header[] = "Accept-Language: en-us,en;q=0.5"; 
+      $header[] = "Accept-Encoding: gzip";
+      $header[] = "Pragma: "; // browsers keep this blank. 
+
+      $curl->get($url,array(),$header);
       $curl->callback = function($response, $info, $request, $error) {
         var_dump($response);
         echo "-------------<br/>";
