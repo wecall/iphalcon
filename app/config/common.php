3e53d91bf1680b5396252b26f45ffd4bee8e5890
ronchen <chenrongrong@51jk.com>
2016-06-15 11:43:16 +0800

    修改信息

--- app/config/common.php
+++ app/config/common.php
@@ -2,10 +2,10 @@
 $config['common'] = array(
 	"session_name" => "access_session",
 	"cryptKey"     => "f8cd1431833d3abcae09cc49f11c7963",
-	// 极验验证
+	// 极验验证 -- 免费版本 / 企业版本 / 企业旗舰版
 	"geetest"      => array(
-		"captcha_id"  => "2626762970a776b451227c8e8e643a43",
-		"private_key" => "a7071f0efa565c28fca2f199e3eb81d2"
+		"captcha_id"  => "4b05a5bc8e547bfba4152080e81b326e",
+		"private_key" => "bc1a648ba9d8c175821331650ea84632"
 	)
 );
 return $config;
