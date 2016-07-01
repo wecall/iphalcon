d4140e8905f7d0d9c42e5362582bdb15333ce930
ronchen <chenrongrong@51jk.com>
2016-07-01 11:09:51 +0800

    修改框架namespace的引用

--- app/config/namespaces.php
+++ app/config/namespaces.php
@@ -1,9 +1,9 @@
 
 <?php
 /* 命名空间配置 */
-$config['index']    = APP_PATH;
-$config['services'] = APP_PATH . 'services/';
-$config['models']   = APP_PATH . 'models/';
-$config['library']  = APP_PATH . 'library/';
+$config['index']    = APP_PATH . '/';
+$config['services'] = APP_PATH . '/services/';
+$config['models']   = APP_PATH . '/models/';
+$config['library']  = APP_PATH . '/library/';
 
 return $config;
