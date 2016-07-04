8be18512d0cd0f4f54bb45bc4294fa9823f4997a
ronchen <chenrongrong@51jk.com>
2016-07-04 15:10:34 +0800

    提交测试邮件服务

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -113,6 +113,7 @@ class TestController extends Controller{
                 // exit;
 
             // 邮件服务测试      ----------------------------  显示成功但是没有收到邮件问题
+            echo "测试邮件服务";
             $mail = new MailsService("qq.email");
             var_dump($mail->sendmail("416994628@163.com","测试邮件地址","<b>文本测试</b>"));
             exit;
