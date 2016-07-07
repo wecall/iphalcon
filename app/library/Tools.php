5e69cf8f3d210542df2948ff7349524536d66358
ronchen <chenrongrong@51jk.com>
2016-07-07 14:18:36 +0800

    处理Github网站采集

--- app/library/Tools.php
+++ app/library/Tools.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * 常用的字符操作，日期操作 ULR操作等等 操作类
  * 
@@ -15,5 +16,22 @@ class Tools {
 			return $pattern;
 
 		return preg_replace('/\\\[px]\{[a-z]\}{1,2}|(\/[a-z]*)u([a-z]*)$/i', "$1$2", $pattern);
+	}
+
+	/**
+	 * 将 css 中style 转变为hash 键值对
+	 * @param "font-size:1.6em;color:red;font-face:文泉驿正黑;"
+	 */
+	public static function strStyleToHash($style){
+		$styleArr = array();
+		foreach (explode(";", $style) as $value) {
+		    if (empty($value)) {
+		        break;
+		    }
+		    $item = explode(":", $value);
+		    $styleArr[$item[0]] =  $item[1];
+		}
+		
+		return $styleArr;
 	}
 }
