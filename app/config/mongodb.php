fefcddeb885276f67034a3cc9a696a6f532865ed
ronchen <chenrongrong@51jk.com>
2016-07-14 11:30:20 +0800

    提交配置

--- app/config/mongodb.php
+++
@@ -1,9 +0,0 @@
-<?php
-//设置默认连接
-$config['host'] = "127.0.0.1";
-$config['port'] = "27017";
-$config['username'] = "chenron";
-$config['password'] = "chenron";
-$config['mongodb']  = "mongodb://localhost:27017/chenron:chenron";
-
-return $config;
